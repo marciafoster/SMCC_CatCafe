8dae12455a95e64d4209cc68da97f47c2f32bcad
shaquala <96317236+shaqfred@users.noreply.github.com>
2023-09-13 11:48:30 -0400

    added data to app.jsx-sf

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const url = 'https://cat-breeds.p.rapidapi.com/cat_breeds';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '06dac55145msh43ca0f365b5b73cp138cdfjsn37f31adbc82f',
+		'X-RapidAPI-Host': 'cat-breeds.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+	 console.log(result);
+} catch (error) {
+	console.error(error);
+}
+
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
