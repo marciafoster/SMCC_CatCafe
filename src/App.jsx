89ee95ed22ceaaa9bee6573d47cb55ad326c7a93
Christine Marchese <chm978@gmail.com>
2023-09-13 14:19:45 -0400

    changes for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 // import { useState } from 'react'
-import './App.css'
+
 import Checkout from './Components/Checkout.jsx'
+import Navbar from "./Components/Navbar.jsx"
+import Pricing from "./Components/Pricing.jsx"
+import Home from "./Components/Home.jsx"
+import About from "./Components/About.jsx"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css'
 
 function App() {
 
@@ -8,7 +14,23 @@ function App() {
 
   return (
     <>
+
+    <Router>
+
     < Checkout />
+    < Navbar />
+
+     <Routes>
+       <Route path="/Home" element={<Home />} />
+       <Route path="/about" element={<About />} />
+       <Route path="/pricing" element={<Pricing />} />
+
+     </Routes>
+
+    </ Router>
+    
+    
+
     </>
   )
 }
