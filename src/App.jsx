afb9364b5a74b4959fa1b642cd95a29fc48c1eac
christophersanchez <christophersanchez@pursuit.org>
2023-09-14 11:11:11 -0400

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { useState } from 'react';
 import './App.css';
 import Checkout from './Components/Checkout.jsx';
-import ErrorPage from './Components/ErrorPage';
+// import ErrorPage from './Components/ErrorPage';
 
 function App() {
-  const [hasError, setHasError] = useState(false);
-  const simulateError = () => {
-    setHasError(true);
-  };
 
   return (
     <>
