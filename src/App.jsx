61a413f39dd4f1f44577174f66e97d8e123dd491
christophersanchez <christophersanchez@pursuit.org>
2023-09-14 11:02:56 -0400

    Updating App.jsx for my ErrorPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-// import { useState } from 'react'
-import './App.css'
-import Checkout from './Components/Checkout.jsx'
+import { useState } from 'react';
+import './App.css';
+import Checkout from './Components/Checkout.jsx';
+import ErrorPage from './Components/ErrorPage';
 
 function App() {
-
- 
+  const [hasError, setHasError] = useState(false);
+  const simulateError = () => {
+    setHasError(true);
+  };
 
   return (
     <>
-    < Checkout />
+      < Checkout />
     </>
   )
 }
